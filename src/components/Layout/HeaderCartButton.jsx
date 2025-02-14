8414b839855c55c639476e9e6cf8c0c85b03565b
Theo <ojw1912@gmail.com>
2025-02-14 10:50:29 +0900

    250214 10:47 # 6. 컨텍스트로 모달 제어하기

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,31 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CartIcon from './CartIcon';
 import styles from './HeaderCartButton.module.scss';
 import CartContext from '../../context/cart-context';
 
-const HeaderCartButton = ({ onShow }) => {
+const HeaderCartButton = () => {
   const { button, icon, badge } = styles;
 
+  // useContext훅은 컨텍스트가 관리하는 데이터를 한번에 가져오는 함수
+  const { openModal } = useContext(CartContext);
+
   return (
-    <CartContext.Consumer>
-      {
-        (x) => {
-          console.log(x);
-          return (
-            <button
-              className={button}
-              onClick={onShow}>
-              <span className={icon}>
-                <CartIcon />
-              </span>
-              <span>My Cart</span>
-              <span className={badge}>3</span>
-            </button>
-          );
-        }
-      }
-      
-    </CartContext.Consumer>
+    <button className={button} onClick={openModal}>
+      <span className={icon}>
+        <CartIcon />
+      </span>
+      <span>My Cart</span>
+      <span className={badge}>3</span>
+    </button>
   );
 };
 
