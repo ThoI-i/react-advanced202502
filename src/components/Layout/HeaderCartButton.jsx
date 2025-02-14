41dd03a1d20a9ab518bbe5c078e734e132053d87
Theo <ojw1912@gmail.com>
2025-02-14 10:14:44 +0900

    250214 10:13 # 6-0. Context.Consumer 방식

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import CartIcon from './CartIcon';
 import styles from './HeaderCartButton.module.scss';
+import CartContext from '../../context/cart-context';
 
 const HeaderCartButton = ({ onShow }) => {
   const { button, icon, badge } = styles;
 
   return (
-    <button className={button} onClick={onShow}>
-      <span className={icon}>
-        <CartIcon />
-      </span>
-      <span>My Cart</span>
-      <span className={badge}>3</span>
-    </button>
+    <CartContext.Consumer>
+      {
+        (x) => {
+          console.log(x);
+          return (
+            <button
+              className={button}
+              onClick={onShow}>
+              <span className={icon}>
+                <CartIcon />
+              </span>
+              <span>My Cart</span>
+              <span className={badge}>3</span>
+            </button>
+          );
+        }
+      }
+      
+    </CartContext.Consumer>
   );
 };
 
